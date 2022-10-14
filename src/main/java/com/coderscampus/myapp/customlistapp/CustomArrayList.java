37d3e6ade1f11dc745e4ce04a0ea6dd757ff711a
JD <jdakemp@gmail.com>
2022-10-14 13:21:14 -0400

    Setup files in folder for project

---
+++ src/main/java/com/coderscampus/myapp/customlistapp/CustomArrayList.java
@@ -0,0 +1,5 @@
+package com.coderscampus.myapp.customlistapp;
+
+public class CustomArrayList {
+
+}
---
+++ src/main/java/com/coderscampus/myapp/customlistapp/CustomListApplication.java
@@ -0,0 +1,10 @@
+package com.coderscampus.myapp.customlistapp;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
