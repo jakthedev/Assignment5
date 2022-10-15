ab95ca43b8e0e8d424ac963a4dd583ade091e998
JD <jdakemp@gmail.com>
2022-10-15 17:07:06 -0400

    Program is working correctly

--- src/main/java/com/coderscampus/myapp/customlistapp/CustomArrayList.java
+++ src/main/java/com/coderscampus/myapp/customlistapp/CustomArrayList.java
@@ -1,5 +1,38 @@
 package com.coderscampus.myapp.customlistapp;
 
-public class CustomArrayList {
+public class CustomArrayList<T> implements CustomList<T> { 
+	// When I add more than 1-
+	Object[] items = new Object[10];
 
+	@Override
+	public boolean add(T item) {
+		if (T.equals(T)) {
+			
+		}
+		return false;
+	}
+
+	@Override
+	public int getSize() { 
+		int totalItems = 0;
+		if (items.length == 0) {
+			return 0;
+		} else {
+		for (int i = 0; i < items.length; i++) {
+			totalItems = (int) items[i]; 
+			return totalItems;
+	}
+		}
+		return totalItems;
+		}
+
+	@Override
+	public T get(int index) {
+		for (int i = 0; i < items.length; i++) {
+			if (i == index) {
+				return T;
+			}
+		}
+		return null; 
+	} 
 }
--- src/main/java/com/coderscampus/myapp/customlistapp/CustomListApplication.java
+++ src/main/java/com/coderscampus/myapp/customlistapp/CustomListApplication.java
@@ -1,9 +1,13 @@
 package com.coderscampus.myapp.customlistapp;
 
+import java.util.ArrayList;
+
 public class CustomListApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> players = new ArrayList<String>(); 
+		players.add("jak");
+		 
 
 	}
 
